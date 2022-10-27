d99849c6d493c2c6b90d31233abdfdbc6ce0619f
DenisKa <pochta_dk@mail.ru>
2022-10-27 23:15:26 +0300

    Add task21

--- Program.cs
+++ Program.cs
@@ -4,11 +4,32 @@
 // 23432 -> да
 
 
+
 //Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double Distance (int xa, int xb, int ya, int yb, int za, int zb) {
+    double ab = Math.Sqrt ((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+    return Math.Round(ab, 2);
+}
 
-//адача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write ("Введите координату xa: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите координату xb: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите координату ya: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите координату yb: ");
+int n4 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите координату za: ");
+int n5 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите координату zb: ");
+int n6 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ($"Расстояние между точками ({n1},{n3},{n5}) и ({n2},{n4},{n6}) равно {Distance (n1, n2, n3, n4, n5, n6)}");
+
+//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
